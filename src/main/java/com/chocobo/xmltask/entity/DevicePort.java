ec3eec1acb4947b8ab093e5ff9d86f537912a5b5
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-04-07 15:11:50 +0300

    entity package init

--- src/main/java/com/chocobo/xmltask/entity/DevicePort.java
+++ src/main/java/com/chocobo/xmltask/entity/DevicePort.java
@@ -1,17 +1,7 @@
 package com.chocobo.xmltask.entity;
 
 public enum DevicePort {
-    COM("COM"),
-    USB("USB"),
-    LPT("LPT");
-
-    private String value;
-
-    DevicePort(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    COM,
+    USB,
+    LPT
 }
